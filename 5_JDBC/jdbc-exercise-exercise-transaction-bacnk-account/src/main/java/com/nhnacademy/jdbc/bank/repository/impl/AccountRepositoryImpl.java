a2f927eaaa097e970f387fe2f42fd92f1393e57f
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-01-31 09:23:30 +0900

    study 0131

--- 5_JDBC/jdbc-exercise-exercise-transaction-bacnk-account/src/main/java/com/nhnacademy/jdbc/bank/repository/impl/AccountRepositoryImpl.java
+++ 5_JDBC/jdbc-exercise-exercise-transaction-bacnk-account/src/main/java/com/nhnacademy/jdbc/bank/repository/impl/AccountRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.nhnacademy.jdbc.bank.repository.impl;
 
 import com.nhnacademy.jdbc.bank.domain.Account;
 import com.nhnacademy.jdbc.bank.repository.AccountRepository;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,16 +10,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
+@Slf4j
 public class AccountRepositoryImpl implements AccountRepository {
 
     public Optional<Account> findByAccountNumber(Connection connection, long accountNumber){
         //todo#1 계좌-조회
+        String sql = "select * from jdbc_account where account_number = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,accountNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                Account account = new Account(resultSet.getLong("account_number"),resultSet.getString("name"),resultSet.getLong("account"));
+                return Optional.of(account);
+            }
+        }catch(SQLException e){
+            log.error("Error findByAccountNumber : {}",e.getMessage());
+        }
+
         return Optional.empty();
     }
 
     @Override
     public int save(Connection connection, Account account) {
         //todo#2 계좌-등록, executeUpdate() 결과를 반환 합니다.
+        String sql = "insert into jdbc_account values(?,?,?)";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,account.getAccountNumber());
+            preparedStatement.setString(2,account.getName());
+            preparedStatement.setLong(3,account.getBalance());
+            return preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            log.error("Error save : {}",e.getMessage());
+        }
         return 0;
     }
 
@@ -26,26 +49,57 @@ public class AccountRepositoryImpl implements AccountRepository {
     public int countByAccountNumber(Connection connection, long accountNumber){
         int count=0;
         //todo#3 select count(*)를 이용해서 계좌의 개수를 count해서 반환
-
+        String sql = "select count(*) from jdbc_account where account_number = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,accountNumber);
+            ResultSet resultSet = preparedStatement.getResultSet();
+            if(resultSet.next()){
+                count = resultSet.getInt(1);
+            }
+        }catch(SQLException e){
+            log.error("Error countByAccountNumber : {}",e.getMessage());
+        }
         return count;
     }
 
     @Override
     public int deposit(Connection connection, long accountNumber, long amount){
         //todo#4 입금, executeUpdate() 결과를 반환 합니다.
-
+        String sql = "update jdbc_account set balance = balance + ? where account_number = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,amount);
+            preparedStatement.setLong(2,accountNumber);
+            return preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            log.error("Error deposit : {}",e.getMessage());
+        }
         return 0;
     }
 
     @Override
     public int withdraw(Connection connection, long accountNumber, long amount){
         //todo#5 출금, executeUpdate() 결과를 반환 합니다.
+        String sql = "update jdbc_account set balance = balance - ? where account_number = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,amount);
+            preparedStatement.setLong(2,accountNumber);
+            return preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            log.error("Error withdraw : {}",e.getMessage());
+        }
         return 0;
     }
 
     @Override
     public int deleteByAccountNumber(Connection connection, long accountNumber) {
         //todo#6 계좌 삭제, executeUpdate() 결과를 반환 합니다.
+        String sql = "delete from jdbc_account where account_number = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setLong(1,accountNumber);
+            return preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            log.error("Error deleteByAccountNumber : {}",e.getMessage());
+        }
         return 0;
     }
 }
--- 5_JDBC/jdbc-exercise-exercise-transaction-bacnk-account/src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ 5_JDBC/jdbc-exercise-exercise-transaction-bacnk-account/src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -14,9 +14,9 @@ public class DbUtils {
     static {
         BasicDataSource basicDataSource = new BasicDataSource();
         //todo#0 {ip},{database},{username},{password} 설정 합니다.
-        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
-        basicDataSource.setUsername("{username}");
-        basicDataSource.setPassword("{password}");
+        basicDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/nhn_academy_0");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("P@ssw0rd");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
